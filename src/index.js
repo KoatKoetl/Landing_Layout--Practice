9b49e1187f6553c85811e8d2a9de2067cae4dc1e
KoatKoetl <stas20027@outlook.com>
2023-11-04 23:05:51 +0200

    Add burgerMenu functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './CSS/style.css';
+import { burgerMenu } from './toggleBurgerMenu';
+
+function enableBurgerMenu() {
+  burgerMenu.toggleBurgerMenu();
+  burgerMenu.hideMenuOnResize();
+  burgerMenu.closeMenu();
+}
+
+enableBurgerMenu();
