a998f5c173890c0779f8eb82b12a1109b9804929
KoatKoetl <stas20027@outlook.com>
2023-11-18 00:28:46 +0200

    5th section done: 1. The same text content at the top of section as an previous. 2. Js carousel changed to set the caorousel by ID. 3. Carousel using the same js. 4. Css is changed also for carousel.

--- src/carousel.js
+++ src/carousel.js
@@ -1,34 +1,33 @@
 class Carousel {
   constructor() {
-    this.slider = document.querySelector('.cards__carousel');
     this.isDown = false;
   }
 
-  activateCarousel() {
+  activateCarousel(carouselId) {
     let startX;
     let scrollLeft;
 
-    this.slider.addEventListener('mousedown', (e) => {
+    carouselId.addEventListener('mousedown', (e) => {
       this.isDown = true;
-      this.slider.classList.add('cards__card--active');
-      startX = e.pageX - this.slider.offsetLeft;
-      scrollLeft = this.slider.scrollLeft;
+      carouselId.classList.add('cards__card--active');
+      startX = e.pageX - carouselId.offsetLeft;
+      scrollLeft = carouselId.scrollLeft;
     });
-    this.slider.addEventListener('mouseleave', (_) => {
+    carouselId.addEventListener('mouseleave', (_) => {
       this.isDown = false;
-      this.slider.classList.remove('cards__card--active');
+      carouselId.classList.remove('cards__card--active');
     });
-    this.slider.addEventListener('mouseup', (_) => {
+    carouselId.addEventListener('mouseup', (_) => {
       this.isDown = false;
-      this.slider.classList.remove('cards__card--active');
+      carouselId.classList.remove('cards__card--active');
     });
-    this.slider.addEventListener('mousemove', (e) => {
+    carouselId.addEventListener('mousemove', (e) => {
       if (!this.isDown) return;
       e.preventDefault();
-      const x = e.pageX - this.slider.offsetLeft;
+      const x = e.pageX - carouselId.offsetLeft;
       const SCROLL_SPEED = 3;
       const walk = (x - startX) * SCROLL_SPEED;
-      this.slider.scrollLeft = scrollLeft - walk;
+      carouselId.scrollLeft = scrollLeft - walk;
     });
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import './CSS/style.css';
 import { burgerMenu } from './headerBurgerMenu';
 import { carousel } from './carousel';
 
+const primaryCarousel = document.querySelector('.cards__carousel');
+const secondaryCarousel = document.querySelector('.testimonials__carousel');
+
 burgerMenu.enableHeaderBurgerMenu();
-carousel.activateCarousel();
+carousel.activateCarousel(primaryCarousel);
+carousel.activateCarousel(secondaryCarousel);
